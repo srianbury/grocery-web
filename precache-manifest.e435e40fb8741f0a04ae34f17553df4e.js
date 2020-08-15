9d1aed101685c5993d387af67fba18c6be9e652b
Brian Sunbury (via Travis CI) <bsunbury29@gmail.com>
2020-08-15 00:20:58 +0000

    Deploy srianbury/grocery-web to github.com/srianbury/grocery-web.git:gh-pages

---
+++ precache-manifest.e435e40fb8741f0a04ae34f17553df4e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "527c58816084c192ef768b8fcadffdcd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a4294ffea196973732f5",
+    "url": "/static/js/2.67ba692e.chunk.js"
+  },
+  {
+    "revision": "77e7f328dacc981fca681ee4ae94d669",
+    "url": "/static/js/2.67ba692e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2531b40934e236a62066",
+    "url": "/static/js/main.66e75ddd.chunk.js"
+  },
+  {
+    "revision": "da8e789c7c083ae0191c",
+    "url": "/static/js/runtime-main.edc115ab.js"
+  }
+]);
