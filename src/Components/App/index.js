e1c4ca02d31917e50779e68e240dd8615dd74d5b
Brian Sunbury <bsunbury29@gmail.com>
2020-08-21 16:56:01 -0700

    top and bottom margin

--- src/Components/App/index.js
+++ src/Components/App/index.js
@@ -1,12 +1,12 @@
-import React from 'react'
-import GroceryMain from '../GroceryMain'
-import Header from '../Header'
+import React from "react";
+import GroceryMain from "../GroceryMain";
+import Header from "../Header";
 
 const App = () => (
-    <div>
-        <Header />
-        <GroceryMain />
-    </div>
-)
+  <div className="mt-2 mb-2">
+    <Header />
+    <GroceryMain />
+  </div>
+);
 
 export default App;
